2bfb2e917d44559e782ac9eec2357f90ec87818c
MuhammadSaifulloh <135417270+bojongpride@users.noreply.github.com>
2024-02-13 18:14:18 +0700

    Add files via upload
    -------------------------------------------------------------------------------------
    Name: Muhammad Saifulloh
    NIM: 220401010207
    -------------------------------------------------------------------------------------
    database added
    program data mahasiswa added

---
+++ Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.nim_search = New System.Windows.Forms.TextBox()
+        Me.button_cari = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(3, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NIM"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'nim_search
+        '
+        Me.nim_search.Location = New System.Drawing.Point(45, 30)
+        Me.nim_search.Name = "nim_search"
+        Me.nim_search.Size = New System.Drawing.Size(139, 20)
+        Me.nim_search.TabIndex = 1
+        '
+        'button_cari
+        '
+        Me.button_cari.Location = New System.Drawing.Point(199, 30)
+        Me.button_cari.Name = "button_cari"
+        Me.button_cari.Size = New System.Drawing.Size(75, 23)
+        Me.button_cari.TabIndex = 2
+        Me.button_cari.Text = "Cari Data"
+        Me.button_cari.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 59)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.ListBox1.Size = New System.Drawing.Size(538, 199)
+        Me.ListBox1.TabIndex = 18
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(280, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(570, 268)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.button_cari)
+        Me.Controls.Add(Me.nim_search)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Data Nilai Mahasiswa"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents nim_search As TextBox
+    Friend WithEvents button_cari As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub button_cari_Click(sender As Object, e As EventArgs) Handles button_cari.Click
+        Dim conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\MuhammadSaifulloh\Downloads\folder onedrive\KULIAH\GANJIL 2023_2024\PEMROGRAMAN VISUAL\UAS\database_mahasiswa.accdb")
+        conn.Open()
+        Dim cmd As New OleDbCommand("select * from Sheet1 where NIM=@parm1", conn)
+        cmd.Parameters.AddWithValue("parm1", nim_search.Text)
+        Dim myreader As OleDbDataReader
+        myreader = cmd.ExecuteReader
+        If myreader.Read Then
+            Dim mystatus As String
+            Dim combinedtext As String
+            If myreader("Nilai") > 70 Then
+                mystatus = "Lulus"
+            Else
+                mystatus = "Gagal"
+            End If
+            combinedtext = String.Concat(myreader("NIM").ToString, " - ", myreader("nama_mahasiswa").ToString, " - ", myreader("kelas").ToString, " - ", myreader("Mata_kuliah").ToString, " - ", "nilai: ", myreader("Nilai"), " - ", "status: ", mystatus)
+            ListBox1.Items.Add(combinedtext)
+            nim_search.Text = ""
+            nim_search.Focus()
+        Else
+            MessageBox.Show("data tidak ditemukan")
+        End If
+        conn.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        nim_search.Text = ""
+        nim_search.Focus()
+    End Sub
+End Class
